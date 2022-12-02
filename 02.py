97a8027ac86941d8e1a52ddef49a40d21f0bd2c9
Daniel <dan.pfister27@gmail.com>
2022-12-02 11:05:23 +0100

    solved day 2 task 2

--- 02.py
+++ 02.py
@@ -11,14 +11,30 @@ inputdata = np.asarray(lines)
 value = {'A': 1, 'B': 2, 'C': 3}
 mapping = {'X': 'A', 'Y': 'B', 'Z': 'C'}
 indexing = {'A': 0, 'B': 1, 'C': 2}
-outcome = np.array([[3, 0, 6], [6, 3, 0], [0, 6, 3]])
+matchup_table = np.array([[3, 0, 6], [6, 3, 0], [0, 6, 3]])
 
-score = 0
+score_1 = 0
 for line in inputdata:
     match = re.match(re.compile(r'(\w)\s(\w)'), line)
     opponent_shape = match.group(1)
     your_shape = mapping[match.group(2)]
 
-    score += outcome[indexing[your_shape]][indexing[opponent_shape]] + value[your_shape]
+    score_1 += matchup_table[indexing[your_shape]][indexing[opponent_shape]] + value[your_shape]
 
-print(f"your final score is {score}")
+print(f"your final score is {score_1}")
+
+############################## PART 1 ##############################
+
+needed = np.array([['C', 'A', 'B'], ['A', 'B', 'C'], ['B', 'C', 'A']])
+outcome_mapping = {'X': 0, 'Y': 1, 'Z': 2}
+ldw_score = [0, 3, 6]
+
+score_2 = 0
+for line in inputdata:
+    match = re.match(re.compile(r'(\w)\s(\w)'), line)
+    opponent_shape = match.group(1)
+    outcome = outcome_mapping[match.group(2)]
+
+    score_2 += value[needed[indexing[opponent_shape]][outcome]] + ldw_score[outcome]
+
+print(f"your final score is {score_2}")
