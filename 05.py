a9a7f6307066621cb7b7b9deac432a13ff470ae0
Dan Pfister <dan.pfister27@gmail.com>
2022-12-05 16:42:48 +0100

    solved day 5 task 2

--- 05.py
+++ 05.py
@@ -10,14 +10,25 @@ for line in lines[:8]:
         if (crate := line[index]) != '':
             stacks[stack] += crate
 
-stacks = np.asarray([stack.strip() for stack in stacks], dtype=object) # 9 stacks, each stack read from top to bottom
+stacks_init = np.asarray([stack.strip() for stack in stacks], dtype=object) # 9 stacks, each stack read from top to bottom
 
 ############################## PART 1 ##############################
 
+stacks = np.copy(stacks_init)
 for line in lines[10:]:
-    match = re.match(re.compile(r'move (\d+) from (\d+) to (\d+)'), line)
+    match = re.match(re.compile(r'move (\d+) from (\d+) to (\d+)'), line) # groups: 1: nr of items, 2: old stack, 3: new stack
     for movement in range(int(match.group(1))):
         stacks[int(match.group(3))-1] = stacks[int(match.group(2))-1][0] + stacks[int(match.group(3))-1] # add item to new stack
         stacks[int(match.group(2))-1] = stacks[int(match.group(2))-1][1:] # delete item from old stack
+
+print(f"the first elements of the stacks are {''.join([stack[0] for stack in stacks])}")
+
+############################## PART 1 ##############################
+
+stacks = np.copy(stacks_init)
+for line in lines[10:]:
+    match = re.match(re.compile(r'move (\d+) from (\d+) to (\d+)'), line)
+    stacks[int(match.group(3))-1] = stacks[int(match.group(2))-1][:int(match.group(1))] + stacks[int(match.group(3))-1]
+    stacks[int(match.group(2))-1] = stacks[int(match.group(2))-1][int(match.group(1)):]
 
 print(f"the first elements of the stacks are {''.join([stack[0] for stack in stacks])}")
