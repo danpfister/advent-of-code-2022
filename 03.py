e5e651cefc19d5cba2fa407e0d5135813724f7ce
Dan Pfister <dan.pfister27@gmail.com>
2022-12-05 11:12:55 +0100

    solved day 3 task 2

--- 03.py
+++ 03.py
@@ -7,14 +7,21 @@ inputdata = np.asarray(lines)
 
 ############################## PART 1 ##############################
 
-priority_sum = 0
+priority_sum_1 = 0
 for line in inputdata:
     compartement_1 = np.asarray([c for c in line[:len(line)//2]])
     compartement_2 = np.asarray([c for c in line[len(line)//2:]])
 
     item = np.intersect1d(compartement_1, compartement_2)[0]
-    priority_sum += ord(item)-96 if item.islower() else ord(item)-38
+    priority_sum_1 += ord(item)-96 if item.islower() else ord(item)-38
     
-print(f"the sum of the priorities is {priority_sum}")
+print(f"the sum of the priorities is {priority_sum_1}")
 
-############################## PART 1 ##############################
+############################## PART 2 ##############################
+
+priority_sum_2 = 0
+for index in range(0, len(inputdata), 3):
+    item = np.intersect1d(np.intersect1d(np.asarray([c for c in inputdata[index]]), np.asarray([c for c in inputdata[index+1]])), np.asarray([c for c in inputdata[index+2]]))[0] # lol
+    priority_sum_2 += ord(item)-96 if item.islower() else ord(item)-38
+
+print(f"the sum of the priorities is {priority_sum_2}")
