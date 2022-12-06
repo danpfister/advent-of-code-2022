42388f300abae32931cfd619b7480acbce00dd81
Daniel <dan.pfister27@gmail.com>
2022-12-06 09:19:11 +0100

    solved day 6 task 2

--- 06.py
+++ 06.py
@@ -8,5 +8,13 @@ inputdata = np.asarray([line.strip() for line in data])[0]
 for index in range(len(inputdata)-3):
     symbols = [c for c in inputdata[index:index+4]]
     if np.unique(symbols).shape[0] == 4:
-        print(f"found marker at index {index+4}")
+        print(f"found packet marker at index {index+4}")
+        break
+
+############################## PART 2 ##############################
+
+for index in range(len(inputdata)-13):
+    symbols = [c for c in inputdata[index:index+14]]
+    if np.unique(symbols).shape[0] == 14:
+        print(f"found message marker at index {index+14}")
         break
