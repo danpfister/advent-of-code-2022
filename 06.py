4cea25ddfc15f1abaf499b83a0da057c8c70faf9
Daniel <dan.pfister27@gmail.com>
2022-12-06 09:25:56 +0100

    repackaged into general function

---
+++ 06.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+data = open(r"input\06.txt", 'r')
+inputdata = np.asarray([line.strip() for line in data])[0]
+
+############################## PART 1&2 ##############################
+
+def find_unique_symbols(characters: str, length: int):
+    for index in range(len(inputdata)-length+1):
+        symbols = [c for c in characters[index:index+length]]
+        if np.unique(symbols).shape[0] == length:
+            return index+length
+
+print(f"found packet marker at index {find_unique_symbols(inputdata, 4)}")
+print(f"found packet marker at index {find_unique_symbols(inputdata, 14)}")
