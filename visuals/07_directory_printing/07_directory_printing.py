260dc7a8501800d6c5d9114787c191560af607ec
Dan Pfister <dan.pfister27@gmail.com>
2022-12-09 15:28:50 +0100

    added emojis

--- visuals/07_directory_printing/07_directory_printing.py
+++ visuals/07_directory_printing/07_directory_printing.py
@@ -2,10 +2,11 @@ import numpy as np
 import re
 import time
 import dill
+import emoji
 
 
 def printerbeepboop(pos: list, previous_pos: int, space: int, current_dir):
-    dir_str =  get_formatted_string(pos, space, current_dir.name)
+    dir_str =  get_formatted_string(pos, space, "\U0001F4C2 " + current_dir.name)
     print(dir_str)
     time.sleep(0.05)
     pos.append(previous_pos)
@@ -13,7 +14,7 @@ def printerbeepboop(pos: list, previous_pos: int, space: int, current_dir):
         printerbeepboop(pos, space, space+15, dir)
     for file in current_dir.files:
         pos.append(space)
-        file_str = get_formatted_string(pos, space+15, file.name)
+        file_str = get_formatted_string(pos, space+15, "\U0001F4D7 "+ file.name)
         print(file_str)
         time.sleep(0.05)
         pos.pop()
@@ -31,6 +32,6 @@ def get_formatted_string(pos: list, name_pos: int, name: str):
     return ''.join(string)
 
 if __name__ == "__main__":
-    data = open(r"visuals\07_directory_printing\root_data.pkl", "rb")
+    data = open(r"visuals\07_directory_printing\root_data.pkl", "rb") # run 07.py first to create pickle file
     root = dill.load(data)
     printerbeepboop([], 0, 0, root)
