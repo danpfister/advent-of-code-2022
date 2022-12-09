1e89bb9b4a1aee2ec8d0d10faaf4524e7fe6ec68
Dan Pfister <dan.pfister27@gmail.com>
2022-12-09 16:02:30 +0100

    improved error handling

--- visuals/07_directory_printing/07_directory_printing.py
+++ visuals/07_directory_printing/07_directory_printing.py
@@ -32,6 +32,10 @@ def get_formatted_string(pos: list, name_pos: int, name: str):
     return ''.join(string)
 
 if __name__ == "__main__":
-    data = open(r"visuals\07_directory_printing\root_data.pkl", "rb") # run 07.py first to create pickle file
+    try:
+        data = open(r"visuals\07_directory_printing\root_data.pkl", "rb") # run 07.py first to create pickle file
+    except:
+        print("file does not exist, you probably need to run 07.py first")
+        raise
     root = dill.load(data)
     printerbeepboop([], 0, 0, root)
