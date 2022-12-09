81f483fd6743a153c75cc3e10c8d1df496b2e0a0
Daniel <dan.pfister27@gmail.com>
2022-12-09 23:05:33 +0100

    fixed typo

--- 09.py
+++ 09.py
@@ -8,7 +8,7 @@ inputdata = np.asarray([line.strip() for line in inputfile])
 
 head_pos = np.zeros(2)
 tail_pos = np.zeros(2)
-visited_pos = []
+visited_pos_tail = []
 head_movement = {"U": [0, 1], "L": [-1, 0], "D": [0, -1], "R": [1, 0]}
 for motion in inputdata:
     match = re.match(re.compile(r"(\w) (\d+)"), motion)
@@ -17,12 +17,12 @@ for motion in inputdata:
         if abs(max(diff := head_pos - tail_pos, key=abs)) > 1: # check if head and tail touching
             tail_pos[0] += min(diff[0], 1) if diff[0] > 0 else max(diff[0], -1)
             tail_pos[1] += min(diff[1], 1) if diff[1] > 0 else max(diff[1], -1)
-        visited_pos.append((tail_pos[0], tail_pos[1]))
-print(f"number of unique positions visited by tail: {len(set(visited_pos))}")
+        visited_pos_tail.append((tail_pos[0], tail_pos[1]))
+print(f"number of unique positions visited by tail: {len(set(visited_pos_tail))}")
 
-############################## PART 1 ##############################
+############################## PART 2 ##############################
 
-visited_pos = []
+visited_pos_last_part = []
 body_pos = np.zeros((10, 2))
 for motion in inputdata:
     match = re.match(re.compile(r"(\w) (\d+)"), motion)
@@ -32,5 +32,5 @@ for motion in inputdata:
             if abs(max(diff := body_pos[body_index] - body_pos[body_index+1], key=abs)) > 1: # check if head and tail touching
                 body_pos[body_index+1][0] += min(diff[0], 1) if diff[0] > 0 else max(diff[0], -1)
                 body_pos[body_index+1][1] += min(diff[1], 1) if diff[1] > 0 else max(diff[1], -1)
-            visited_pos.append((body_pos[-1][0], body_pos[-1][1]))
-print(f"number of unique positions visited by last body part: {len(set(visited_pos))}")
+            visited_pos_last_part.append((body_pos[-1][0], body_pos[-1][1]))
+print(f"number of unique positions visited by last body part: {len(set(visited_pos_last_part))}")
