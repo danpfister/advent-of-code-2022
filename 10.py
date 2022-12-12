4b5bc55f7d8b22bfce8ee84a5cddf816777fb294
Dan Pfister <dan.pfister27@gmail.com>
2022-12-12 14:19:29 +0100

    solved day 10 task 2

--- 10.py
+++ 10.py
@@ -4,25 +4,33 @@ import re
 inputfile = open(r".\input\10.txt", "r")
 inputdata = np.asarray([line.strip() for line in inputfile])
 
-############################## PART 1 ##############################
-
 class Cpu():
     def __init__(self, interesting_signals: list) -> None:
         self.interesting_signals = interesting_signals
         self.register_value = 1
         self.cycle = 0
         self.interesting_signals_strengths = 0
+        self.output = []
     
     def __repr__(self) -> str:
-        return f"end of cycle {self.cycle} with register value {self.register_value}"
+        return f"during cycle {self.cycle} the register value is {self.register_value}"
     
     def next_cycle(self, value: int):
         self.cycle += 1
-        self.register_value += value
         if self.cycle in self.interesting_signals:
-            self.interesting_signals_strengths += (self.cycle + 1) * self.register_value
+            self.interesting_signals_strengths += self.cycle * self.register_value # value during cycle
+        if self.register_value-1 <= (self.cycle-1)%40 and (self.cycle-1)%40 <= self.register_value+1: # during cycle k, the CRT is drawing on pixel k-1
+            self.output.append('#')
+        else:
+            self.output.append('.')
+        if self.cycle%40 == 0:
+            self.output.append('\n')
+        print(self)
+        self.register_value += value
 
-interesting_signals = [19, 59, 99, 139, 179, 219] # after the cycle before is equal to during the next cycle
+############################## PART 1 ##############################
+
+interesting_signals = [20, 60, 100, 140, 180, 220]
 cpu = Cpu(interesting_signals)
 for instruction in inputdata:
     if instruction == "noop":
@@ -32,3 +40,7 @@ for instruction in inputdata:
     cpu.next_cycle(0)
     cpu.next_cycle(int(match.group(1)))
 print(f"the sum of the interesting signal strengths is {cpu.interesting_signals_strengths}")
+
+############################## PART 2 ##############################
+
+print(''.join(cpu.output))
