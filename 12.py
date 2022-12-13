466615daf4ff90a82d04d5b38b919858c4d333cd
Dan <dan.pfister27@gmail.com>
2022-12-13 22:32:50 +0100

    minor improvements

--- 12.py
+++ 12.py
@@ -12,7 +12,8 @@ inputdata[end[0]][end[1]] = 'z'
 distances = np.zeros_like(inputdata, dtype=np.int32)
 
 def bfs(inputdata, start, end, distances):
-    visited = [start]
+    visited = np.zeros_like(inputdata, dtype=bool)
+    visited[start[0]][start[1]] = True
     queue = [start]
 
     distances[start[0]][start[1]] = 0
@@ -20,7 +21,7 @@ def bfs(inputdata, start, end, distances):
         current = queue.pop(0)
 
         if np.array_equal(current, end):
-            print(f"finished with number of steps {distances[current[0]][current[1]]}")
+            return distances[current[0]][current[1]]
             break
 
         for neighbor in np.asarray([current + direction for direction in [[0, 1], [1, 0], [0, -1], [-1, 0]]]):
@@ -28,36 +29,38 @@ def bfs(inputdata, start, end, distances):
                 continue
             if ord(inputdata[neighbor[0]][neighbor[1]]) > ord(inputdata[current[0]][current[1]]) + 1: # check if neighbor not too high
                 continue
-            if np.any(np.asarray([np.array_equal(neighbor, np.asarray(already_visited)) for already_visited in visited])): # check if neighbor already visited
+            current_visit = np.zeros_like(visited, dtype=bool); current_visit[neighbor[0]][neighbor[1]] = True
+            if np.any(np.bitwise_and(visited, current_visit)): # check if already visited
                 continue
             distances[neighbor[0]][neighbor[1]] = distances[current[0]][current[1]] + 1
-            visited.append(neighbor)
+            visited = np.bitwise_or(visited, current_visit)
             queue.append(neighbor)
         
-        print(distances)
+        #print(distances)
 
-#bfs(inputdata, start, end, distances)
+print(f"found shortest path from current position with distance {bfs(inputdata, start, end, distances)}")
 
 def reverse_bfs(inputdata, start):
-    visited = [start]
+    visited = np.zeros_like(inputdata, dtype=bool)
+    visited[start[0]][start[1]] = True
     queue = [start]
     while len(queue) > 0:
         current = queue.pop(0)
 
         if inputdata[current[0]][current[1]] == 'a':
-            print(f"finished with number of steps {distances[current[0]][current[1]]}")
-            break
+            return current
 
         for neighbor in np.asarray([current + direction for direction in [[0, 1], [1, 0], [0, -1], [-1, 0]]]):
             if neighbor[0] not in range(inputdata.shape[0]) or neighbor[1] not in range(inputdata.shape[1]): # check if neighbor out of bounds
                 continue
             if ord(inputdata[neighbor[0]][neighbor[1]]) < ord(inputdata[current[0]][current[1]]) - 1: # check if neighbor not too high
                 continue
-            if np.any(np.asarray([np.array_equal(neighbor, np.asarray(already_visited)) for already_visited in visited])): # check if neighbor already visited
+            current_visit = np.zeros_like(visited, dtype=bool); current_visit[neighbor[0]][neighbor[1]] = True
+            if np.any(np.bitwise_and(visited, current_visit)): # check if already visited
                 continue
-            visited.append(neighbor)
+            visited = np.bitwise_or(visited, current_visit)
             queue.append(neighbor)
-    return current
+    raise Exception("end was never reached!")
 
 distances = np.zeros_like(inputdata, dtype=np.int32)
-bfs(inputdata, reverse_bfs(inputdata, end), end, distances)
+print(f"found shortest path from any a with distance {bfs(inputdata, reverse_bfs(inputdata, end), end, distances)}")
