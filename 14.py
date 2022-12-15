893e80b4edee3a01aa0e89c7a158e126f8e99317
Dan Pfister <dan.pfister27@gmail.com>
2022-12-15 09:45:22 +0100

    solved day 14 task 1

--- 14.py
+++ 14.py
@@ -2,7 +2,7 @@ import numpy as np
 import re
 
 def draw_cave(paths):
-    cave = np.full((10, 10), '.') # first index is top to bottom, second index is left to right 
+    cave = np.full((250, 1000), '.') # first index is top to bottom, second index is left to right 
     for path in paths:
         previous_corner = None
         for corner in path:
@@ -19,7 +19,7 @@ def draw_cave(paths):
     return cave
 
 def drop_sand(cave) -> bool:
-    current_pos = np.array([0, 6])
+    current_pos = np.array([0, 500])
     while True:
         if current_pos[0] == cave.shape[0]-1: # dropped into abyss
             return False
@@ -35,14 +35,14 @@ def drop_sand(cave) -> bool:
         else:
             break
     cave[current_pos[0], current_pos[1]] = 'o'
-    print(cave)
     return True
     
     
 if __name__ == "__main__":
-    inputfile = open(r".\input\14_test.txt", 'r')
+    inputfile = open(r".\input\14.txt", 'r')
     inputdata = np.asarray([line.strip() for line in inputfile])
-    paths = [[[int(corner[1]), int(corner[0]) - 494] for corner in re.findall(re.compile(r'(\d+),(\d+)(?: -> )*'), path)] for path in inputdata]
+    paths = [[[int(corner[1]), int(corner[0])] for corner in re.findall(re.compile(r'(\d+),(\d+)(?: -> )*'), path)] for path in inputdata]
+    print(paths)
 
     cave = draw_cave(paths)
     nr_of_sandblocks = 0
