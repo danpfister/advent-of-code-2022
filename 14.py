5865091440ce868f6fbbc6ee84a363eea4dd721d
Dan Pfister <dan.pfister27@gmail.com>
2022-12-19 10:38:45 +0100

    solved day 14 task 2

--- 14.py
+++ 14.py
@@ -1,7 +1,8 @@
 import numpy as np
 import re
 
-def draw_cave(paths):
+def draw_cave(paths: list): # TODO could improve this by making cave size dynamic
+    '''draw the paths into the cave matrix'''
     cave = np.full((250, 1000), '.') # first index is top to bottom, second index is left to right 
     for path in paths:
         previous_corner = None
@@ -18,10 +19,13 @@ def draw_cave(paths):
     
     return cave
 
-def drop_sand(cave) -> bool:
+def drop_sand(cave: np.ndarray, part: int) -> bool:
+    '''drop a block of sand,
+    returns:    for part 1: false if sand dropped into abyss, true else
+                for part 2: false if sands final position is at the initial position, true else'''
     current_pos = np.array([0, 500])
     while True:
-        if current_pos[0] == cave.shape[0]-1: # dropped into abyss
+        if current_pos[0] == cave.shape[0]-1 and part == 1: # dropped into abyss
             return False
         if cave[current_pos[0]+1, current_pos[1]] == '.':
             current_pos += [1, 0]
@@ -35,19 +39,39 @@ def drop_sand(cave) -> bool:
         else:
             break
     cave[current_pos[0], current_pos[1]] = 'o'
+    if part == 1: # return true if part 1
+        return True
+    if np.array_equal(current_pos, [0, 500]): # for part 2 return false if the sands final position is the initial position
+        return False
     return True
+
+def get_dimensions(paths: list):
+    max_depth = 0
+    for path in paths:
+        for corner in path:
+            max_depth = max(max_depth, corner[0])
+    return max_depth
     
     
 if __name__ == "__main__":
     inputfile = open(r".\input\14.txt", 'r')
     inputdata = np.asarray([line.strip() for line in inputfile])
     paths = [[[int(corner[1]), int(corner[0])] for corner in re.findall(re.compile(r'(\d+),(\d+)(?: -> )*'), path)] for path in inputdata]
-    print(paths)
 
     cave = draw_cave(paths)
     nr_of_sandblocks = 0
-    while drop_sand(cave):
+    while drop_sand(cave, 1):
         nr_of_sandblocks += 1
         continue
+    
+    print(nr_of_sandblocks)
+    
+    paths.append([[get_dimensions(paths)+2, 0], [get_dimensions(paths)+2, 999]])
+    cave = draw_cave(paths)
+    nr_of_sandblocks = 0
+    while True: # TODO fix this temp workaround the off-by-one bug (drop_sand() returns false one round "too early")
+        nr_of_sandblocks += 1
+        if not drop_sand(cave, 2):
+            break
     
     print(nr_of_sandblocks)
