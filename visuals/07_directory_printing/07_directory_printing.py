9a98b09bdb7c3b832fa5b3d5f00437147c79b8ae
Dan Pfister <dan.pfister27@gmail.com>
2022-12-23 13:53:44 +0100

    added capturing output to file

--- visuals/07_directory_printing/07_directory_printing.py
+++ visuals/07_directory_printing/07_directory_printing.py
@@ -1,22 +1,19 @@
 import numpy as np
-import re
-import time
 import dill
-import emoji
+from pathlib import Path
 
 
 def printerbeepboop(pos: list, previous_pos: int, space: int, current_dir):
     dir_str =  get_formatted_string(pos, space, "\U0001F4C2 " + current_dir.name)
-    print(dir_str)
-    time.sleep(0.05)
+    global output
+    output += dir_str + "\n"
     pos.append(previous_pos)
     for dir in current_dir.dirs:
-        printerbeepboop(pos, space, space+15, dir)
+        printerbeepboop(pos, space, space+17, dir)
     for file in current_dir.files:
         pos.append(space)
         file_str = get_formatted_string(pos, space+15, "\U0001F4D7 "+ file.name)
-        print(file_str)
-        time.sleep(0.05)
+        output += file_str + "\n"
         pos.pop()
     pos.pop()
     return
@@ -28,10 +25,11 @@ def get_formatted_string(pos: list, name_pos: int, name: str):
     string = list(f"{name:->{name_pos}}")
     string[:pos[-1]] = pos[-1] * [' ']
     for i in pos:
-        string[i] = "'"
+        string[i] = "|"
     return ''.join(string)
 
 if __name__ == "__main__":
+    output = ""
     try:
         data = open(r"visuals\07_directory_printing\root_data.pkl", "rb") # run 07.py first to create pickle file
     except:
@@ -39,3 +37,7 @@ if __name__ == "__main__":
         raise
     root = dill.load(data)
     printerbeepboop([], 0, 0, root)
+    path = Path(__file__).parent
+    file = open(path / 'printed_directory.txt', 'w', encoding='utf-8')
+    file.write(output)
+    file.close()
