3fdc76df45000839d48694ba43e40628d6092157
Dan <dan.pfister27@gmail.com>
2023-11-20 18:32:25 +0100

    solved day 1 in cpp

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,16 +1,43 @@
 #include "../Helpers/file_utils.h"
+#include <algorithm>
 
-void part1() {
-    std::cout << "part 1 placeholder" << std::endl;
+void part1(std::vector<std::string>& input) {
+    std::vector<int> calories;
+    int current = 0;
+    for (const auto & item : input) {
+        if (item.size() == 0) {
+            calories.push_back(current);
+            current = 0;
+            continue;
+        }
+        current += std::stoi(item);
+    }
+    
+    std::sort(calories.begin(), calories.end(), std::greater<int>());
+
+    std::cout << "The elf carrying the most calories is carrying " << calories[0] << " calories." << std::endl;
 }
 
-void part2() {
+void part2(std::vector<std::string> input) {
+    std::vector<int> calories;
+    int current = 0;
+    for (const auto & item : input) {
+        if (item.size() == 0) {
+            calories.push_back(current);
+            current = 0;
+            continue;
+        }
+        current += std::stoi(item);
+    }
     
+    std::sort(calories.begin(), calories.end(), std::greater<int>());
+
+    std::cout << "The elves carrying the most calories are carrying " << calories[0] + calories[1] + calories[2] << " calories." << std::endl;
 }
 
 int main() {
-    part1();
-    std::vector<std::string> contents = FileReader::readFile("C:/Users/Daniel/Github/advent-of-code-2022/day01/01.txt");
-    std::cout << contents[0] << std::endl;
+    std::vector<std::string> input = FileReader::readFile("C:/Users/Daniel/Github/advent-of-code-2022/day01/01.txt");
+    part1(input);
+    part2(input);
     return 0;
 }
